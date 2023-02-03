e44a9da7a1bdead7e6742ab3d6cb3028da56aee7
mak9606 <alikhan1996116@gmail.com>
2023-02-03 09:46:20 +0500

    bug fixes

--- routes/private/user.js
+++ routes/private/user.js
@@ -1,26 +1,30 @@
-const express = require("express");
+const express = require('express');
 const {
   createUserObjFromToken,
-} = require("../../helpers/users/createUserObjFromToken");
-const { User } = require("../../models/user");
+} = require('../../helpers/users/createUserObjFromToken');
+const { User } = require('../../models/user');
 
 const router = express.Router();
 
-router.post("/handleProfile", async (req, res, next) => {
+router.post('/register', async (req, res, next) => {
   const { decodedToken } = req.body;
-  console.log(decodedToken, "decodedToken");
 
-  let targetUser = await User.findById(decodedToken.uid);
-  console.log(targetUser, "targetUser");
-  if (targetUser) {
-    return res.status(200).send(targetUser);
-  }
   let userObj = createUserObjFromToken(decodedToken);
   let newUser = await User.create(userObj);
   if (!newUser) {
-    return res.status(401).send("User Not Created Successfully");
+    return res.status(401).send('User Not Created Successfully');
   }
   return res.status(200).send(newUser);
 });
+router.get('/login', async (req, res, next) => {
+  const { decodedToken } = req.body;
+  let targetUser = await User.findById(decodedToken.uid);
+  console.log(targetUser, 'targetUser');
+  if (targetUser) {
+    return res.status(200).send(targetUser);
+  }
+  // else{
 
+  // }
+});
 module.exports = router;
